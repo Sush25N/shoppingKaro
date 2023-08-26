1653885a0abfe5d97def60c44b6e6b9a6e12c41b
Sush25N <sushgabhane123@gmail.com>
2023-08-27 02:25:01 +0530

    added product component

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Container } from 'react-bootstrap'
 import Header from "./components/Header.jsx"
 import Footer from './components/Footer.jsx'
+import { Outlet } from 'react-router-dom'
 
 const App = () => {
   return (
@@ -9,7 +10,7 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
-          <h1>Welcome To Shopping_Karo</h1>
+          <Outlet />
         </Container>
       </main>
       <Footer />
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,24 +1,41 @@
-import React from 'react'
-import {Navbar, Nav, Container} from 'react-bootstrap';
-import {FaShoppingCart, FaUser} from "react-icons/fa"
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { FaShoppingCart, FaUser } from "react-icons/fa";
+import { LinkContainer } from "react-router-bootstrap";
+import logo from "../assets/logo.png";
+import "./header.css";
 
 const Header = () => {
   return (
     <header>
-      <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
+      <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
         <Container>
-            <Navbar.Brand href='/'>Shopping_Karo</Navbar.Brand>
-            <Navbar.Toggle aria-controls='basic-navbar-nav'/>
-            <Navbar.Collapse id='basic-navbar-nav'>
-                <Nav className='ms-auto'>
-                    <Nav.Link href='/cart'><FaShoppingCart /> Cart </Nav.Link>
-                    <Nav.Link href='/login'><FaUser /> Sign IN </Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
+          <LinkContainer to="/">
+            <Navbar.Brand>
+              <img className="logo_img" src={logo} alt="Shopping_Karo" />
+              Shopping_Karo
+            </Navbar.Brand>
+          </LinkContainer>
+
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <FaShoppingCart /> Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to=".login">
+                <Nav.Link>
+                  <FaUser /> Sign IN
+                </Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
