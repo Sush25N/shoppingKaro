4a948c83aace1b5c6db06fd779bcac97983f5ac9
Sush25N <sushgabhane123@gmail.com>
2023-09-07 22:47:38 +0530

    productController

---
+++ backend/controllers/productController.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const productController = () => {
+  return (
+    <div>
+      {/* Controller logic will be written here */}
+    </div>
+  )
+}
+
+export default productController
