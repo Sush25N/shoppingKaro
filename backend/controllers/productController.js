c2996ecfc077d34b995365562a6086aa7bce1646
Sush25N <sushgabhane123@gmail.com>
2023-09-09 22:36:29 +0530

    fetching products

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import asyncHandler from '../middleware/asyncHandler.js';
+import Product from '../models/productModel.js';
 
-const productController = () => {
-  return (
-    <div>
-      {/*Product Controller logic will be written here */}
-    </div>
-  )
-}
 
-export default productController
+// Fetch all products
+
+const getProducts = asyncHandler(async (req, res) => {
+  const pageSize = process.env.PAGINATION_LIMIT;
+  const page = Number(req.query.pageNumber) || 1;
+
+  const keyword = req.query.keyword
+    ? {
+        name: {
+          $regex: req.query.keyword,
+          $options: 'i',
+        },
+      }
+    : {};
+
+  const count = await Product.countDocuments({ ...keyword });
+  const products = await Product.find({ ...keyword })
+    .limit(pageSize)
+    .skip(pageSize * (page - 1));
+
+  res.json({ products, page, pages: Math.ceil(count / pageSize) });
+});
+
+
+// Fetch single product
+
+const getProductById = asyncHandler(async (req, res) => {
+
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    return res.json(product);
+  } else {
+    res.status(404);
+    throw new Error('Product not found');
+  }
+});
